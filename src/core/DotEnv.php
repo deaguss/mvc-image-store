773bfc834c0f7e6519347398c124bdb752c1d3d9
deagus <dedeaskyt121@gmail.com>
2023-12-13 10:02:18 +0800

    add comment

--- src/core/DotEnv.php
+++ src/core/DotEnv.php
@@ -17,18 +17,21 @@ class DotEnv {
             throw new \InvalidArgumentException(sprintf("File '%s' not readable", $this -> path));
         }
 
+        // Membaca file baris per baris
         $lines = file($this -> path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
         foreach($lines as $line){
+            // Melewati baris yang dimulai dengan karakter '#'
             if(strpos(trim($line), '#') === 0){
                 continue;
             }
 
-
+            // Memisahkan nama variabel dan nilainya
             list($name , $value) = explode('=', $line,2);
             $name = trim($name);
             $value = trim($value);
 
+            // Mengatur variabel lingkungan dan variabel server jika belum ada
             if(!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)){
                 putenv(sprintf('%s=%s', $name, $value));
                 $_ENV[$name] = $value;
@@ -37,3 +40,5 @@ class DotEnv {
         } 
     }
 }
+
+?>
--- src/core/Sanitization.php
+++ src/core/Sanitization.php
@@ -3,65 +3,74 @@
 namespace App\Core;
 
 class Sanitization {
-    const FIlTER = [
+    // Daftar filter yang dapat digunakan
+    const FILTER = [
         'string' => FILTER_SANITIZE_SPECIAL_CHARS,
         'string[]' => [
             'filter' => FILTER_SANITIZE_SPECIAL_CHARS,
             'flags' => FILTER_REQUIRE_ARRAY
-          ],
-          'email' => FILTER_SANITIZE_EMAIL,
-          'int' => [
+        ],
+        'email' => FILTER_SANITIZE_EMAIL,
+        'int' => [
             'filter' => FILTER_SANITIZE_NUMBER_INT,
             'flags' => FILTER_REQUIRE_SCALAR
-          ],
-          'int[]' => [
+        ],
+        'int[]' => [
             'filter' => FILTER_SANITIZE_NUMBER_INT,
             'flags' => FILTER_REQUIRE_ARRAY
-          ],
-          'float' => [
+        ],
+        'float' => [
             'filter' => FILTER_SANITIZE_NUMBER_FLOAT,
             'flags' => FILTER_FLAG_ALLOW_FRACTION
-          ],
-          'float[]' => [
+        ],
+        'float[]' => [
             'filter' => FILTER_SANITIZE_NUMBER_FLOAT,
             'flags' => FILTER_REQUIRE_ARRAY
-          ],
-          'url' => FILTER_SANITIZE_URL
-        ];
+        ],
+        'url' => FILTER_SANITIZE_URL
+    ];
 
-        private function array_trim(array $items): array {
-            return array_map(function ($item) {
-                if(is_string($item)){
-                    return trim($item);
-                } elseif(is_array($item)){
-                    return $this->array_trim($item);
-                } else {
-                    return $item;
+    // Fungsi untuk menghilangkan spasi pada setiap elemen array
+    private function array_trim(array $items): array {
+        return array_map(function ($item) {
+            if (is_string($item)) {
+                return trim($item);
+            } elseif (is_array($item)) {
+                return $this->array_trim($item);
+            } else {
+                return $item;
+            }
+        }, $items);
+    }
+
+    // Fungsi utama untuk membersihkan dan memvalidasi input
+    public function sanitize(
+        array $inputs,
+        array $fields = [],
+        int $default_filter = FILTER_SANITIZE_SPECIAL_CHARS,
+        array $filters = self::FILTER,
+        bool $trim = true
+    ) {
+        // Jika ada field yang harus dihandle secara khusus
+        if ($fields) {
+            foreach ($fields as $key => $field) {
+                // Jika field adalah string, hapus tag HTML
+                if ($field == 'string' && isset($inputs[$key])) {
+                    $tempvar = strip_tags($inputs[$key]);
+                    $inputs[$key] = $tempvar;
                 }
-            }, $items); 
-        }
-
-        public function sanitize(
-            array $inputs,
-            array $fields = [],
-            int $default_filter = FILTER_SANITIZE_SPECIAL_CHARS,
-            array $filters = self::FIlTER,
-            bool $trim = true
-        ){
-            if($fields){
-                foreach($fields as $key => $field){
-                    if($field == 'string' && isset($inputs[$key])){
-                        $tempvar = strip_tags($inputs[$key]);
-                        $inputs[$key] = $tempvar;   
-                    }
-                }
-
-                $options = array_map(fn($field) => $filters[trim($field)], $fields);
-                $data = filter_var_array($inputs, $options);
-            }else {
-                $data = filter_var_array($inputs, $default_filter);    
             }
 
-            return $trim ? $this->array_trim($data) : $data;    
+            // Membuat daftar filter sesuai dengan fields
+            $options = array_map(fn ($field) => $filters[trim($field)], $fields);
+            // Membersihkan dan memvalidasi input menggunakan filter yang telah ditentukan
+            $data = filter_var_array($inputs, $options);
+        } else {
+            // Jika tidak ada field yang harus dihandle secara khusus, gunakan filter default
+            $data = filter_var_array($inputs, $default_filter);
         }
+
+        // Menghilangkan spasi pada setiap elemen array jika trim diaktifkan
+        return $trim ? $this->array_trim($data) : $data;
+    }
 }
