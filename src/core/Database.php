070e25c8b169e58335b7cef6e88ca1a4cbab0980
deagus <dedeaskyt121@gmail.com>
2023-12-13 10:12:01 +0800

    create getter setter table, comlum dan create connection to db

---
+++ src/core/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $conn;
+    private $tableName;
+    private $column = [];
+
+    public function __construct() {
+        $this->conn = $this->setConnection();       
+    }
+
+    public function setTableName($tableName) {
+        $this->tableName = $tableName;
+    }
+
+    public function setColumn($column) {
+        $this->column = $column;
+    }
+    
+    protected function setConnection(){
+        try {
+            $host = getenv('DB_HOST');
+            $user = getenv('DB_USER');
+            $password = getenv('DB_PASSWORD');
+            $db = getenv('DB_NAME');
+            $port = getenv('DB_PORT');
+
+            $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            return $conn;       
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());  
+        }
+    }
+}
